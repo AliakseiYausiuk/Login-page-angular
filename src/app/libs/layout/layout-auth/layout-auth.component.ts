b8e71db8e437b2b452a372235b9fc39d52ea4322
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-10-01 16:33:24 +0300

    feat: add error-message

--- src/app/libs/layout/layout-auth/layout-auth.component.ts
+++ src/app/libs/layout/layout-auth/layout-auth.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {BtnComponent} from "../../component-ui/components/btn/btn.component";
 import {AuthFormComponent} from "../../auth/auth-form/auth-form.component";
+import {ErrorMessageComponent} from "../../component-ui/components/error-message/error-message.component";
 
 @Component({
   selector: 'L-layout-auth',
   standalone: true,
   imports: [
     BtnComponent,
-    AuthFormComponent
+    AuthFormComponent,
+    ErrorMessageComponent
   ],
   templateUrl: './layout-auth.component.html',
   styleUrl: './layout-auth.component.scss'
 })
 export class LayoutAuthComponent {
 
+  isError: boolean = false;
+
+  onError(value: boolean) {
+    this.isError = value;
+  }
 }
