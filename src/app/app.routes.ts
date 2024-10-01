97085f9527fbc5cba73df33534c1e1e5592e75ef
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-10-01 09:43:15 +0300

    feat: add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LayoutAuthComponent} from "./libs/layout/layout-auth/layout-auth.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutAuthComponent
+  }
+];
