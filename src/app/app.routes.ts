813fec729886e7d531d03d1b5f133182f7fdb8cf
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-10-01 15:33:06 +0300

    fix: add canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import {LayoutAuthComponent} from "./libs/layout/layout-auth/layout-auth.component";
+import {ProfileComponent} from "./libs/profile/profile/profile.component";
+import {canActivateAuth} from "./libs/auth/data/access.guard";
 
 export const routes: Routes = [
   {
     path: '',
+    component: ProfileComponent,
+    canActivate: [canActivateAuth]
+  },
+  {
+    path: 'login',
     component: LayoutAuthComponent
-  }
+  },
+
 ];
