0f2b22291ec6d4cd6b388201a294af4c19251dde
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-10-01 13:49:14 +0300

    feat: add layout-auth component

--- src/app/libs/layout/layout-auth/layout-auth.component.ts
+++ src/app/libs/layout/layout-auth/layout-auth.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {BtnComponent} from "../../component-ui/components/btn/btn.component";
+import {AuthFormComponent} from "../../auth/auth-form/auth-form.component";
 
 @Component({
   selector: 'L-layout-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    BtnComponent,
+    AuthFormComponent
+  ],
   templateUrl: './layout-auth.component.html',
   styleUrl: './layout-auth.component.scss'
 })
